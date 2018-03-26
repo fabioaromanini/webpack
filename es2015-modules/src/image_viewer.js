4cbc2547f3cfe3784d865b4030d31f37475505ba
Romanini <Fabio.Romanini@vistajet.com>
2018-03-26 15:27:25 -0300

    adds code splitting

--- es2015-modules/src/image_viewer.js
+++ es2015-modules/src/image_viewer.js
@@ -1,13 +1,14 @@
-import large from '../assets/large.png';
+// import large from '../assets/large.png';
 import small from '../assets/small.png';
 
 import '../styles/image-viewer.css'
 
-const img = document.createElement('img');
-img.src = small;
-document.body.appendChild(img);
+export default () => {
+	const img = document.createElement('img');
+	img.src = small;
+	document.body.appendChild(img);
+};
 
-const large_img = document.createElement('img');
-large_img.src = large;
-
-document.body.appendChild(large_img);
+// const large_img = document.createElement('img');
+// large_img.src = large;
+// document.body.appendChild(large_img);
--- es2015-modules/src/index.js
+++ es2015-modules/src/index.js
@@ -1,4 +1,16 @@
-import sum from './math';
-import './image_viewer';
+// Code for bundle and loaders
 
-console.log(sum(2, 2));
+// import sum from './math';
+// import './image_viewer';
+
+// console.log(sum(2, 2));
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+	System.import('./image_viewer').then(module => {
+		module.default();
+	});
+};
+
+document.body.appendChild(button);
